476e5f2b2417ae7f190adb6b0fb8d408dcd247af
kiruba3441 <kirubakaran.d@gmail.com>
2016-03-29 03:42:54 +0000

    amazon interview questions @ careercup

---
+++ src/amazon_ccup/StringifyInteger.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class StringifyInteger{
+    
+  public String stringify(Integer val){
+    if(val!=null){
+      boolean isNegative = val<0;
+      val = Math.abs(val);    
+      Stack<Integer> intStack = new Stack<Integer>();     
+      while(val>0){
+        int rem = val%10;
+        val = val/10;
+        intStack.push(val);
+      }
+      StringBuffer intStringBuffer = new StringBuffer();
+      if(isNegative)
+        intStringBuffer.append("-");
+      String[] intStringValues = ["0","1","2","3","4","5","6","7","8","9"];
+      while(!intStack.empty()){
+          intStringBuffer.append(intStringValues[intStack.pop()]);
+      }
+      return intStringBuffer.toString();  
+    }  
+    return "";
+  }
+  public static void main(String[] args){
+      StringifyInteger sti = new StringifyInteger();
+      String result = sti.strinfigy(-123);
+      System.out.println(result);
+      String result = sti.strinfigy(1235);
+      System.out.println(result);
+  }
+}
